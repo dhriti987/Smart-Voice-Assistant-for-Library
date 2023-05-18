d3162298e1322fb859b65743f436141425b6dec8
Dhritiman <dhritimanbhardwaj@gmail.com>
2023-05-19 00:03:46 +0530

    minor changes

--- app/blah.py
+++
@@ -1,7 +0,0 @@
-def cypher(text: str, key):
-    # text = text.replace(' ', '')
-    d = {key[i]: text[i::len(key)] for i in range(len(key))}
-    return "".join([d[key] for key in sorted(list(d.keys()))])
-
-
-print(cypher("Geeks for Geeks", "HACK"))
